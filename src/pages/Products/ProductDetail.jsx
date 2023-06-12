879b5797445bcfee238143485e52ec709677697a
CarlosCrisonG <carloscrison@gmail.com>
2023-06-12 06:32:25 -0500

    Correcciones sugeridas

--- src/pages/Products/ProductDetail.jsx
+++ src/pages/Products/ProductDetail.jsx
@@ -48,64 +48,65 @@ function PorductDetail() {
     return (
         <>
             {Object.keys(product).length > 0 ?
-            <div className="general-container">
-                <main className="product-detail-main-container">
-                    <section className="main-section">
-                        <div className="title">
-                            <img className="icon" src="/icons/productos-black.png" alt="icon"></img>
-                            <h1 className="titulo-producto-mobile">Detalle del producto</h1>
-                        </div>
-                        <div className="main-top-section">
-                            {/* <!-- Carrusel --> */}
-                            <section id="carrusel">
-                                <div className="slideshow-container">
-                                    <button className="prev" onClick={prevSlide}>&#10094;</button>
-                                    <img src={product.imagesUrl[slides]} style={{ width: "100%" }} />
-                                    <button className="next" onClick={nextSlide}>&#10095;</button>
-                                </div>
-                            </section>
-                            <section className="right-box">
-                                <div className="product-cost-box">
-                                    <div className="separador">
-                                        <label className="label">Titulo:</label>
-                                        <span className="titulo-producto-desktop">{product.name}</span>
+                <div className="general-container">
+                    <main className="product-detail-main-container">
+                        <section className="main-section">
+                            <div className="title">
+                                <img className="icon" src="/icons/productos-black.png" alt="icon"></img>
+                                <h1 className="titulo-producto-mobile">Detalle del producto</h1>
+                            </div>
+                            <div className="main-top-section">
+                                {/* <!-- Carrusel --> */}
+                                <section id="carrusel">
+                                    <div className="slideshow-container">
+                                        <button className="prev" onClick={prevSlide}>&#10094;</button>
+                                        <img src={product.imagesUrl[slides]} />
+                                        <button className="next" onClick={nextSlide}>&#10095;</button>
                                     </div>
-                                    <div className="separador">
-                                        <label className="label">Precio:</label>
-                                        <span className="precio">{product.price}</span>
+                                </section>
+                                <section className="right-box">
+                                    <div className="product-cost-box">
+                                        <div className="separador">
+                                            <label className="label">Titulo:</label>
+                                            <span className="titulo-producto-desktop">{product.name}</span>
+                                        </div>
+                                        <div className="separador">
+                                            <label className="label">Precio:</label>
+                                            <span className="precio">{product.price}</span>
+                                        </div>
+                                        <div className="separador">
+                                            <label className="label">Categoria:</label>
+                                            <span className="precio">{product.category.name}</span>
+                                        </div>
+                                        <div className="color separador">
+                                            <label className="label">Colores:</label>
+                                            <ul>
+                                                {product.colors.map((color, j) => {
+                                                    return <li key={j}>{color.name} </li>
+                                                })}
+                                            </ul>
+                                        </div>
+                                        <p className="cuotas">Stock disponible</p>
+                                        <p className="envio">Envio gratis</p>
                                     </div>
-                                    <div className="separador">
-                                        <label className="label">Categoria:</label>
-                                        <span className="precio">{product.category.name}</span>
-                                    </div>
-                                    <div className="color separador">
-                                        <label className="label">Colores:</label>
-                                        <ul>
-                                            {product.colors.map((color, j) => {
-                                                return <li key={j}>{color.name} </li>
-                                            })}
-                                        </ul>
-                                    </div>
-                                    <p className="cuotas">Stock disponible</p>
-                                    <p className="envio">Envio gratis</p>
-                                </div>
-                                <div className="product-descr-box">
-                                    <h4 className="subtitle-descripcion">Descripción</h4>
-                                    <p className="texto">{product.description}</p>
-                                </div>
-                            </section>
-                        </div>
-                        <div className="product-caract-box">
-                            <h4 className="subtitle-caracteristicas">Características</h4>
-                            <ul>
-                                {product.features.map((feature, i) => {
-                                    return <li className="caracteristicas" key={i}>{feature.name}</li>
-                                })}
-                            </ul>
-                        </div>
-                    </section>
-                </main>
-            </div>
+                                </section>
+                            </div>
+                            {/* <!-- Descripción y Características --> */}
+                            <div className="product-descr-box">
+                                <h4 className="subtitle-descripcion">Descripción</h4>
+                                <p className="texto">{product.description}</p>
+                            </div>
+                            <div className="product-caract-box">
+                                <h4 className="subtitle-caracteristicas">Características</h4>
+                                <ul>
+                                    {product.features.map((feature, i) => {
+                                        return <li className="caracteristicas" key={i}>{feature.name}</li>
+                                    })}
+                                </ul>
+                            </div>
+                        </section>
+                    </main>
+                </div>
                 :
                 <div className="general-container">
                     <TailSpin
